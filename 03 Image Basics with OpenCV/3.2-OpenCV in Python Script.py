2f9cd497d71b024fa8df51ce23c7d959ff684b8f
Abhimanyu Singh <abhimanyus1997@gmail.com>
2022-07-26 17:44:15 +0530

    Updated the scale for Image

--- 03 Image Basics with OpenCV/3.2-OpenCV in Python Script.py
+++ 03 Image Basics with OpenCV/3.2-OpenCV in Python Script.py
@@ -8,8 +8,13 @@ path = "../res/puppy.jpg"
 img = cv2.imread(path)
 
 resize_scale = 0.1
+h,w, _ = img.shape
+h_scaled, w_scaled = round(h*resize_scale), round(w*resize_scale)
 
-#cv2.imshow("Puppy", img)
+#Scalling Down/Up the image
+img = cv2.resize(img, (w_scaled, h_scaled))
+
+print("Press Q to exit")
 while True:
     cv2.imshow("Puppy Frame", img)
 
